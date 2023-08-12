b39e93dd2ac0e3fa3bc83b1c504f7077e7832090
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2023-08-12 11:22:20 +0530

    bootstap and navbar

--- green-spoon/src/App.js
+++ green-spoon/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+        <Router>
+          <Nav />
+          <div className="container">
+            <Routes>
+              {/* <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} /> */}
+            </Routes>
+          </div>
+        </Router>
+    
+    </>
   );
 }
 
