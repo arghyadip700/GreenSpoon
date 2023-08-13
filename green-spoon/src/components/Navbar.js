7e42c3d9cb35e27bedbb1153571845b1b8215796
Arghyadip Dhara <arghyadipdhara1@gmail.com>
2023-08-13 13:52:31 +0530

    About Page updated

--- green-spoon/src/components/Navbar.js
+++ green-spoon/src/components/Navbar.js
@@ -24,42 +24,26 @@ function Navbar() {
     <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Green Spoon</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">CommonHome</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/rest_home">RestHome</a>
+          <a className="nav-link active" aria-current="page" href="/rest_home">Restaurant</a>
         </li>  
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/ngo_home">NgoHome</a>
+          <a className="nav-link active" aria-current="page" href="/ngo_home">NGO</a>
         </li> 
-
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link active" aria-current="page" href="/about">About Us</a>
         </li>
-        <li className="nav-item dropdown">
-       
-        </li>
-        
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-
-
-
-
-        
         {isAuthenticated?(
-         
-          
   <li class="nav-item d-flex">
-     
      <li className='m-2 text-success fw-bold'>{user.email}</li> 
   <button type="button" class="btn btn-dark" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
 Log Out
