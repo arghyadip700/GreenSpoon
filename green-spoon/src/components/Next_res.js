6b30f4d122ef58e82ecbde2e01f2af05f300dc7e
saheli <plsaheli123@gmail.com>
2023-08-13 12:19:13 +0530

    next_res updated

--- green-spoon/src/components/Next_res.js
+++ green-spoon/src/components/Next_res.js
@@ -1,41 +1,78 @@
-
-
-
-
-import React from 'react'
-
+import React from "react";
+import don from "./all-img/donation.jpg";
 export default function Next_res() {
   return (
-    <div>
-      <center><h2><u>Add Food</u></h2></center>
-      <form  method="post" id="FOOD">
-	<h4>
-	<div class="field">
-		<label for="items">Items:</label>
-		<input type="text" id="name" name="name" placeholder="Add your food items" />
-		<small></small>
-	</div>
-	<div class="field">
-		<label for="amount">Amount:</label>
-		<input type="text" id="amount" name="amount" placeholder="Enter amount" />
-		<small></small>
-	</div>
-	< div class = "input-container" >
-		<label for="expiry time">Expiry time:</label>
-		
-		
-		<input type="text" id="expiry time" name="expiry time"  placeholder="Enter  expected expiry time" required />
-		<small></small>
-	</div>
-	</h4>
-	<br></br>
+    <>
+      <div className="Rest-home-page" style={{ width: "100%" }}>
+        <div
+          style={{
+            margin: 0,
+            width: "100%",
+            padding: "0",
+            background: "black",
+            overflow: "hidden",
+            position: "relative",
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          <img
+            src={don}
+            className="d-block w-100"
+            alt="..."
+            style={{ opacity: "0.4", objectFit: "cover", height: "100vh" }}
+          />
+          <div className="center-text">
+		  <div className="Next-res-container">
+        <center>
+          <h2>
+            <u>Add Food</u>
+          </h2>
+        </center>
+        <form method="post" id="FOOD">
+          <h4>
+            <div class="field">
+              <label for="items">Items:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Add your food items"
+              />
+              <small></small>
+            </div>
+            <div class="field">
+              <label for="amount">Amount:</label>
+              <input
+                type="text"
+                id="amount"
+                name="amount"
+                placeholder="Enter amount"
+              />
+              <small></small>
+            </div>
+            <div class="input-container">
+              <label for="expiry time">Expiry time:</label>
 
-	<button type="submit">ADD</button>
-	
-</form>
+              <input
+                type="text"
+                id="expiry time"
+                name="expiry time"
+                placeholder="Enter  expected expiry time"
+                required
+              />
+              <small></small>
+            </div>
+          </h4>
+          <br></br>
 
-
+          <button type="submit">ADD</button>
+        </form>
       </div>
-  )
+          </div>
+        </div>
+      </div>
+      
+    </>
+  );
 }
-
