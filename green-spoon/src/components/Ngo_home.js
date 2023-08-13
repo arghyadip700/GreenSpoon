46c04603c19269a4e5eb13fdbe526bdd8f4655c9
Arghyadip Dhara <arghyadipdhara1@gmail.com>
2023-08-13 12:43:10 +0530

    updated NGO Home

--- green-spoon/src/components/Ngo_home.js
+++ green-spoon/src/components/Ngo_home.js
@@ -4,30 +4,6 @@ function Ngo_home() {
   return (
     <>
       <div className="Ngo-home-page " style={{ width: "100%" }}>
-        <div
-          style={{
-            margin: 0,
-            width: "100%",
-            padding: "0",
-            background: "black",
-            overflow: "hidden",
-            position: "relative",
-            textAlign: "center",
-            color: "white",
-          }}
-        >
-          {/* <img
-            src={NGO}
-            className="d-block  "
-            alt="..."
-            style={{ opacity: "0.4", objectFit: "cover", height: "100%" }}
-          /> */}
-          <div className="center-text">
-            “There's enough on this planet for everyone's needs but not if you
-            keep wasting.”
-          </div>
-        </div>
-
         <div className="allrest d-flex w-100">
           <div className="w-20 d-flex justify-content-center side">
             <div className="restallfood w-100">FOODS AVAILABLE</div>
@@ -96,12 +72,7 @@ function Ngo_home() {
           </div>
         </div>
       </div>
- <div className='Ngo-home-page' style={{ width: "100%" }}>
-                <div style={{ margin: 0, width: "100%", padding: "0", background: "black", overflow: "hidden", position: "relative", textAlign: "center", color: "white" }}>
-                    <img src={NGO} className="d-block w-100 " alt="..." style={{ opacity: "0.4", objectFit: "cover", height: "100vh" }} />
-                    <div className='center-text'>“There's enough on this planet for everyone's needs but not if you keep wasting.”</div>
-                </div>
-            </div>
+
             <div className="restContainer">
           </div>
     </>
